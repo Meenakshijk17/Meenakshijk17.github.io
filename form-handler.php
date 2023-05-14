a8ea705100f6148fb6403662dc851b1d63bf7a12
Meenakshijk17 <63089206+Meenakshijk17@users.noreply.github.com>
2023-05-14 00:19:09 -0400

    first commit

---
+++ form-handler.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Email for your website
+$email_from = "contact@";
+$email_subject = "New Message";
+$email_body = "Visitor Name: $name.\n".
+              "Visitor Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "Message: $message.\n";
+$to_email = 'meenakshi.krishnakumar.mk@gmail.com';
+
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to_email, $email_subject, $email_body, $headers)
+header("Location: contact.html")
+?>
